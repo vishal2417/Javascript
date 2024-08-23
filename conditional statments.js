e416903f8279214cf1817a5c8381ebf8ba9121a7
Vishal Sharma <vishalsharma96q@gmail.com>
2024-08-23 10:54:04 +0530

    Add files via upload

---
+++ conditional statments.js
@@ -0,0 +1,27 @@
+/*let age = 10;
+if(age >= 18){
+    console.log("you can vote");}
+
+else{
+    console.log("you can't");
+}*/
+/* let mode = "light";
+ let color;
+ if (mode=="dark"){
+   console.log("dark");
+ }
+ else{
+    console.log("light");
+ }*/
+ //ODD/Even
+ /*let no =10;
+ if (no%2===0){
+    console.log("even");
+ }
+ 
+ else{
+    console.log("odd");
+ }*/
+
+    
+
